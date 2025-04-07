441611de071fd77ccefa3c5d64deea0cc58b2dd0
athrun0001 <athrun0001@aol.com>
2025-04-07 19:23:30 +0800

    update on TFD Hotfix 1.2.13b

--- dllmain.cpp
+++ dllmain.cpp
@@ -1880,7 +1880,7 @@ DWORD WINAPI Init(HMODULE Module)
 		//int32_t GObjOffsetRelative = *reinterpret_cast<int32_t*>(GObjOffsetAddress);
 		//uintptr_t GObjAddress = (GObjsPtr + 7) + GObjOffsetRelative;
 		//uintptr_t GObjeOffset = GObjAddress - GameModule.dwBase;
-		TFD_SDK::Offsets::GObjects = 0x9E87C20;
+		TFD_SDK::Offsets::GObjects = 0x9E86C50;
 #ifdef IS_DEBUG
 		std::cout << "DescentInternal - Found GObjects at: " << std::hex << GObjsPtr << std::dec << "\n";
 		Sleep(1000);
